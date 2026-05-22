88b988f57b6412c488e5fffe372696253d19d3bf
Aline-Pinotti <alinefpinotti@gmail.com>
2026-05-22 13:15:42 -0300

    feat(user): add user entity

---
+++ api/src/main/java/com/orderflow/ecommerce/entities/User.java
@@ -0,0 +1,74 @@
+package com.orderflow.ecommerce.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "tb_user")
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false, unique = true)
+    private String email;
+    private String password;
+
+
+    // Customer information for Invoices
+    /**
+     * CPF or CNPJ
+     */
+    @Column(name = "tax_id", nullable = false, unique = true, length = 20)
+    private String taxId;
+
+    /**
+     * Estadual registration (IE)
+     */
+    @Column(length = 30)
+    private String stateRegistration;
+
+    private String phone;
+    private LocalDate birthDate;
+
+    /**
+     * Used in Invoices (NF-e)
+     */
+    private Boolean taxpayerType;
+
+    /**
+     * Google API
+     */
+    @Column(unique = true)
+    private String googleId;
+
+    /**
+     * private Address address;
+     */
+    @Column(length = 40)
+    private String street;
+    @Column(length = 40)
+    private String complement;
+    @Column(length = 10)
+    private String number;
+    @Column(length = 40)
+    private String neighborhood;
+    @Column(length = 40)
+    private String city;
+    @Column(length = 40)
+    private String country;
+    @Column(length = 2)
+    private String state;
+    @Column(length = 10)
+    private String zipCode;
+
+}
