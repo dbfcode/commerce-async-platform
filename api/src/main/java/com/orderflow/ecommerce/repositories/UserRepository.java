b4a54a63915353600cef1c644bec10da0ed7cdc1
Aline-Pinotti <alinefpinotti@gmail.com>
2026-05-22 13:28:09 -0300

    feat(user): add user repository

---
+++ api/src/main/java/com/orderflow/ecommerce/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.orderflow.ecommerce.repositories;
+
+import com.orderflow.ecommerce.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmailIgnoreCase(String email);
+
+}
